abccdfb9c6d8118ad07a4c864c4723e5e1858ded
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-08-05 08:53:31 -0700

    Program That Finds The Factorial Of A Number

--- ForLoop/PrimeNumber.js
+++ ForLoop/PrimeNumber.js
@@ -3,7 +3,7 @@ let num = readline.questionInt("Enter a number: ");
 let count = 0;
 for(let i = 2; i <= num/2; i++){
     if(num%i == 0){
-        count++;
+        count++;node 
         break;
     }
 }
